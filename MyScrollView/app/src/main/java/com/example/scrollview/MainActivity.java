c801c309cc237e57d58537b9a8364d75d7b4f315
Sunghee Kim <50495214+xxunghee@users.noreply.github.com>
2021-01-04 11:09:20 +0900

    Layout 20-12-29

---
+++ MyScrollView/app/src/main/java/com/example/scrollview/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.scrollview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+    }
+
+    public void onButtonCliked(TextView tv) {
+        addOne();
+    }
+
+    public void addOne() {
+        textView.
+    }
+}
