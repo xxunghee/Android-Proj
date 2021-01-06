67156b150bc4da227d0dd44cdf25fbab7207feae
xxunghee <xxunghee@gmail.com>
2021-01-06 18:05:40 +0900

    NavigateDrawerActivity 21-01-06

--- MyDrawer/app/src/main/java/com/example/drawer/MainActivity.java
+++ MyDrawer/app/src/main/java/com/example/drawer/MainActivity.java
@@ -39,9 +39,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
-                .setDrawerLayout(drawer)
-                .build();
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow).setDrawerLayout(drawer).build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
