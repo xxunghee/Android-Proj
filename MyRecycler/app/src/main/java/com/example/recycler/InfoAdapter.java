7a8b66a37959509b440e498d84dafac998cb6b4a
xxunghee <xxunghee@gmail.com>
2021-01-15 14:50:32 +0900

    21-01-15 DataBinding 추가

--- MyRecycler/app/src/main/java/com/example/recycler/InfoAdapter.java
+++ MyRecycler/app/src/main/java/com/example/recycler/InfoAdapter.java
@@ -1,10 +1,7 @@
 package com.example.recycler;
 
-import android.content.Context;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
