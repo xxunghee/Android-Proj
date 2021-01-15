a218db278194dbd58d81805597efa3b075dc3f9f
xxunghee <xxunghee@gmail.com>
2021-01-15 14:37:42 +0900

    21-01-15 DataBinding 추가

--- MyRecycler/app/src/main/java/com/example/recycler/InfoAdapter.java
+++ MyRecycler/app/src/main/java/com/example/recycler/InfoAdapter.java
@@ -9,10 +9,11 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.recycler.databinding.ItemInfoBinding;
+
 import java.util.ArrayList;
 
 public class InfoAdapter extends RecyclerView.Adapter<InfoAdapter.ViewHolder> {
-
     //---- 데이터가 저장될 배열
     private ArrayList<Info> data = null;
 
@@ -23,13 +24,19 @@ public class InfoAdapter extends RecyclerView.Adapter<InfoAdapter.ViewHolder> {
 
     //---- 아이템 뷰를 저장하고 화면에 보여주는 ViewHolder 클래스 생성
     static class ViewHolder extends RecyclerView.ViewHolder {
+        ItemInfoBinding binding;
+
         private TextView nameView;
         private TextView ageView;
 
-        ViewHolder(View itemView) {
-            super(itemView);
-            nameView = (TextView) itemView.findViewById(R.id.tv_name);
-            ageView = (TextView) itemView.findViewById(R.id.tv_age);
+        ViewHolder(ItemInfoBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+
+        public void bind(Info info) {
+            binding.tvName.setText(info.getName());
+            binding.tvAge.setText(info.getAge());
         }
     }
 
@@ -37,22 +44,17 @@ public class InfoAdapter extends RecyclerView.Adapter<InfoAdapter.ViewHolder> {
     @NonNull
     @Override
     public InfoAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        //---- 인플레이션,
-        Context context = parent.getContext();
-        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View view = inflater.inflate(R.layout.item_info, parent, false);
+        //---- 인플레이션
+        ItemInfoBinding binding = ItemInfoBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false);
 
-        //---- 아이템 뷰를 위한 ViewHolder 객체 생성하여 리턴
-        InfoAdapter.ViewHolder viewHolder = new InfoAdapter.ViewHolder(view);
-        return viewHolder;
+        return new ViewHolder(binding);
     }
 
     //---- 바인딩하여 데이터를 아이템 뷰에 표시
     @Override
     public void onBindViewHolder(@NonNull InfoAdapter.ViewHolder holder, int position) {
         Info info = data.get(position);
-        holder.nameView.setText(info.getName());
-        holder.ageView.setText(info.getAge());
+        holder.bind(info);
     }
 
     //---- 전체 데이터 수 리턴
--- MyRecycler/app/src/main/java/com/example/recycler/MainActivity.java
+++ MyRecycler/app/src/main/java/com/example/recycler/MainActivity.java
@@ -1,19 +1,25 @@
 package com.example.recycler;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.recycler.databinding.ActivityMainBinding;
+
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding  binding;
+    InfoAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
         ArrayList<Info> list = new ArrayList<Info>();
         list.add(new Info("ABC", 1));
@@ -27,14 +33,13 @@ public class MainActivity extends AppCompatActivity {
         list.add(new Info("YZ", 9));
 
         //---- RecyclerView에 LayoutManager 지정
-        RecyclerView recyclerView = findViewById(R.id.recyclerView);
         /** LinearLayout의 방향(기본: vertical)을 horizontal로 변경할 때
          *  recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)) ;
          */
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        binding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         //---- RecyclerView에 Adapter 지정
-        InfoAdapter adapter = new InfoAdapter(list);
-        recyclerView.setAdapter(adapter);
+        adapter = new InfoAdapter(list);
+        binding.recyclerView.setAdapter(adapter);
     }
 }
