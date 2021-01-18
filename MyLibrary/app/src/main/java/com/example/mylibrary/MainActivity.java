5e7b4bd29b20e018018ffc51537158c809d70df6
xxunghee <xxunghee@gmail.com>
2021-01-18 11:23:06 +0900

    DataBinding 적용

--- MyLibrary/app/src/main/java/com/example/mylibrary/MainActivity.java
+++ MyLibrary/app/src/main/java/com/example/mylibrary/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.mylibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import com.example.mylibrary.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
     }
 }
