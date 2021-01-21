eaf7ae3c2ca669cfceaced27539fd37e2f129078
xxunghee <xxunghee@gmail.com>
2021-01-21 15:36:10 +0900

    DataBinding 적용

--- MyListView/app/src/main/java/com/example/mylistview/MainActivity.java
+++ MyListView/app/src/main/java/com/example/mylistview/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.mylistview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import com.example.mylistview.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
     }
 }
