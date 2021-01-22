1e04d40ac1775ae6b3ed0bb310c391fcabadf76e
xxunghee <xxunghee@gmail.com>
2021-01-22 10:18:24 +0900

    21-01-22 Fragment
    MainActivity에 생성한 버튼(button2)으로 프래그먼트 간 전환 가능하도록 수정

--- MyFragment/app/src/main/java/com/example/fragment/MainActivity.java
+++ MyFragment/app/src/main/java/com/example/fragment/MainActivity.java
@@ -3,10 +3,14 @@ package com.example.fragment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     MainFragment mainFragment;
     MenuFragment menuFragment;
+    Button button;
+    int cnt = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +19,15 @@ public class MainActivity extends AppCompatActivity {
 
         mainFragment = (MainFragment)getSupportFragmentManager().findFragmentById(R.id.mainFragment);
         menuFragment = new MenuFragment();
+
+        button = findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cnt++;
+                onFragmentChanged(cnt%2);
+            }
+        });
     }
 
     public void onFragmentChanged(int index) {
