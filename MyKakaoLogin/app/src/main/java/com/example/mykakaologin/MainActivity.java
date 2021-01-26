fb5fe85a3a4eb0372801c70911cd467eec396b9c
xxunghee <xxunghee@gmail.com>
2021-01-26 17:12:04 +0900

    21-01-26 KakaoLogin
    minSdkVersion 수정

--- MyKakaoLogin/app/src/main/java/com/example/mykakaologin/MainActivity.java
+++ MyKakaoLogin/app/src/main/java/com/example/mykakaologin/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.mykakaologin;
 
 import androidx.appcompat.app.AppCompatActivity;
+import com.kakao.sdk.common.util.Utility;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.kakao.sdk.common.KakaoSdk;
 
 public class MainActivity extends AppCompatActivity {
+    final String NATIVE_APP_KEY = "1251c57fc0dfd79671115a05f3749210";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // 카카오 SDK 초기화
-        KakaoSdk.init(this, "{NATIVE_APP_KEY}");
+        KakaoSdk.init(this, NATIVE_APP_KEY);
+
+        Log.d("TAG", Utility.INSTANCE.getKeyHash(this));
     }
 }
