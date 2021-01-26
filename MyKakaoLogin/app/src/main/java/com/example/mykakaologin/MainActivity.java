f3342e977b531b00748161e2c7bd7ae6124caf0c
xxunghee <xxunghee@gmail.com>
2021-01-26 17:23:34 +0900

    21-01-26 KakaoLogin
    https://developers.kakao.com/docs/latest/ko/kakaologin/android
    + 카카오 계정으로 로그인 구현 중!!

--- MyKakaoLogin/app/src/main/java/com/example/mykakaologin/MainActivity.java
+++ MyKakaoLogin/app/src/main/java/com/example/mykakaologin/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.mykakaologin;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.kakao.sdk.auth.LoginClient;
 import com.kakao.sdk.common.util.Utility;
 
 import android.os.Bundle;
@@ -9,7 +11,7 @@ import android.util.Log;
 import com.kakao.sdk.common.KakaoSdk;
 
 public class MainActivity extends AppCompatActivity {
-    final String NATIVE_APP_KEY = "1251c57fc0dfd79671115a05f3749210";
+    final String NATIVE_APP_KEY = "INSERT YOUR NATIVE APP KEY";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +21,6 @@ public class MainActivity extends AppCompatActivity {
         // 카카오 SDK 초기화
         KakaoSdk.init(this, NATIVE_APP_KEY);
 
-        Log.d("TAG", Utility.INSTANCE.getKeyHash(this));
+        LoginClient.getInstance().loginWithKakaoAccount(this);
     }
 }
