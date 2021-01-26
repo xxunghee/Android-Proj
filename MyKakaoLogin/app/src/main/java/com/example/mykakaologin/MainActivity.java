1ee31c1724a6e6bc2508b16126facc4ba32c50e0
xxunghee <xxunghee@gmail.com>
2021-01-26 16:48:12 +0900

    21-01-26 KakaoLogin
    + 초기화

--- MyKakaoLogin/app/src/main/java/com/example/mykakaologin/MainActivity.java
+++ MyKakaoLogin/app/src/main/java/com/example/mykakaologin/MainActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.kakao.sdk.common.KakaoSdk;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 카카오 SDK 초기화
+        KakaoSdk.init(this, "{NATIVE_APP_KEY}");
     }
 }
