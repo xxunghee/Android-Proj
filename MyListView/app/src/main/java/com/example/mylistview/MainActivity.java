921ee318a808fe1ccd373559f73c4edab5193f9c
xxunghee <xxunghee@gmail.com>
2021-01-27 13:45:57 +0900

    21-01-27 ListView
    + 멤버 설명 추가

--- MyListView/app/src/main/java/com/example/mylistview/MainActivity.java
+++ MyListView/app/src/main/java/com/example/mylistview/MainActivity.java
@@ -39,13 +39,13 @@ public class MainActivity extends AppCompatActivity {
         ninizs.add(new Niniz("빠냐", R.drawable.bbanya));
 
         comments = new ArrayList<>();
-        comments.add("스카피는");
-        comments.add("죠르디는");
-        comments.add("앙몬드는");
-        comments.add("케로와 베로니는");
-        comments.add("콥은");
-        comments.add("팬다 주니어는");
-        comments.add("빠냐는");
+        comments.add("스노우타운 최고 인기 셰프, 스카피\n귀여운 외모와 달리 북극곰 시절\n영광의 상처(X)를 간직한 듬직한 토끼이다.");
+        comments.add("떠내려 온 빙하에서 깨어난 공룡, 죠르디\n빛나는 과거를 뒤로한 채\n지금은 취업을 꿈꾸며 하루하루 열심히 산다.");
+        comments.add("초콜릿 잼을 달고 사는 하프물범, 앙몬드\n매사에 느긋하지만 초콜릿 앞에서는\n빛보다 빠르게 움직인다.");
+        comments.add("장난꾸러기 쌍둥이 펭귄 남매 케로베로니\n가끔 투닥거려도 항상 붙어다니며\n따뜻한 불 옆에 있는 것을 좋아한다.");
+        comments.add("빠냐의 탐정 콤비, 콥\n호기심이 많아 소소한 사건에도 관심을 갖고\n자칭 스노우타운 탐정으로 활동한다.");
+        comments.add("스노우 타운에 불시착한 외계인, 팬다\n흥이 많고 어디서나 주목 받고 싶어\n파티라면 빠지지 않는다.");
+        comments.add("콥의 탐정 콤비, 빠냐\n호기심이 많아 소소한 사건에도 관심을 갖고\n자칭 스노우타운 탐정으로 활동한다.");
 
         //--- Adapter 설정
         adapter = new ListViewAdapter(ninizs);
