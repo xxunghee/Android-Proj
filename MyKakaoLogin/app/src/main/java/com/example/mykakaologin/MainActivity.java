5268fe9ce699c11fd59b16fe2e5b68261acaf4f0
xxunghee <xxunghee@gmail.com>
2021-01-27 17:35:50 +0900

    21-01-27 KakaoLogin
    https://youngest-programming.tistory.com/93 소스
    !! redirectLoginActivity 왜 뜨는지 이유 찾기

--- MyKakaoLogin/app/src/main/java/com/example/mykakaologin/MainActivity.java
+++ MyKakaoLogin/app/src/main/java/com/example/mykakaologin/MainActivity.java
@@ -1,26 +1,175 @@
 package com.example.mykakaologin;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import com.kakao.sdk.auth.LoginClient;
-import com.kakao.sdk.common.util.Utility;
-
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 
-import com.kakao.sdk.common.KakaoSdk;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.kakao.auth.ErrorCode;
+import com.kakao.auth.ISessionCallback;
+import com.kakao.auth.Session;
+import com.kakao.network.ErrorResult;
+import com.kakao.usermgmt.UserManagement;
+import com.kakao.usermgmt.callback.MeResponseCallback;
+import com.kakao.usermgmt.response.model.UserProfile;
+import com.kakao.util.exception.KakaoException;
+import com.kakao.util.helper.log.Logger;
+
+import java.security.MessageDigest;
+import java.util.HashMap;
+import java.util.Map;
+
+
+// ### 잘찾아요
 
 public class MainActivity extends AppCompatActivity {
-    final String NATIVE_APP_KEY = "INSERT YOUR NATIVE APP KEY";
+    private SessionCallback callback;      //콜백 선언
+    //유저프로필
+    String token = "";
+    String name = "";
+
+
+    final static String TAG = "LoginActivityT";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
-        // 카카오 SDK 초기화
-        KakaoSdk.init(this, NATIVE_APP_KEY);
+        //카카오 로그인 콜백받기
+        callback = new SessionCallback();
+        Session.getCurrentSession().addCallback(callback);
+        //키값 알아내기(알아냈으면 등록하고 지워도 상관없다)
+        getAppKeyHash();
 
-        LoginClient.getInstance().loginWithKakaoAccount(this);
+        //자기 카카오톡 프로필 정보 및 디비정보 쉐어드에 저장해놨던거 불러오기
+        loadShared();
+    }
+
+    //카카오 디벨로퍼에서 사용할 키값을 로그를 통해 알아낼 수 있다. (로그로 본 키 값을을 카카오 디벨로퍼에 등록해주면 된다.)
+    private void getAppKeyHash() {
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md;
+                md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                String something = new String(Base64.encode(md.digest(), 0));
+                Log.e("Hash key", something);
+            }
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            Log.e("name not found", e.toString());
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (Session.getCurrentSession().handleActivityResult(requestCode, resultCode, data)) {
+            return;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Session.getCurrentSession().removeCallback(callback);
+    }
+
+    private class SessionCallback implements ISessionCallback {
+
+        @Override
+        public void onSessionOpened() {
+            requestMe();
+            //redirectSignupActivity();  // 세션 연결성공 시 redirectSignupActivity() 호출
+        }
+
+        @Override
+        public void onSessionOpenFailed(KakaoException exception) {
+            if (exception != null) {
+                Logger.e(exception);
+            }
+            setContentView(R.layout.activity_main); // 세션 연결이 실패했을때
+        }                                            // 로그인화면을 다시 불러옴
+    }
+
+
+    protected void requestMe() { //유저의 정보를 받아오는 함수
+        UserManagement.requestMe(new MeResponseCallback() {
+            @Override
+            public void onFailure(ErrorResult errorResult) {
+                String message = "failed to get user info. msg=" + errorResult;
+                Logger.d(message);
+
+                ErrorCode result = ErrorCode.valueOf(errorResult.getErrorCode());
+                if (result == ErrorCode.CLIENT_ERROR_CODE) {
+                    finish();
+                } else {
+                    redirectLoginActivity();
+                }
+            }
+
+            @Override
+            public void onSessionClosed(ErrorResult errorResult) {
+                redirectLoginActivity();
+            }
+
+            @Override
+            public void onNotSignedUp() {
+            } // 카카오톡 회원이 아닐 시 showSignup(); 호출해야함
+
+            @Override
+            public void onSuccess(final UserProfile userProfile) {  //성공 시 userProfile 형태로 반환
+                Logger.d("UserProfile : " + userProfile);
+                Log.d(TAG, "유저가입성공");
+                // Create a new user with a first and last name
+                // 유저 카카오톡 아이디 디비에 넣음(첫가입인 경우에만 디비에저장)
+
+                Map<String, String> user = new HashMap<>();
+                user.put("token", userProfile.getId() + "");
+                user.put("name", userProfile.getNickname());
+
+                redirectHomeActivity(); // 로그인 성공시 MainActivity로
+
+            }
+        });
+    }
+
+    private void redirectHomeActivity() {
+        Log.d("###","redirectHomeActivity");
+//        startActivity(new Intent(this, HomeActivity.class));
+//        finish();
+    }
+
+    protected void redirectLoginActivity() {
+        Log.d("###","redirectLoginActivity");
+//        final Intent intent = new Intent(this, LoginActivity.class);
+//        intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+//        startActivity(intent);
+//        finish();
+    }
+
+    /*쉐어드에 입력값 저장*/
+    private void saveShared( String id, String name) {
+        SharedPreferences pref = getSharedPreferences("profile", MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putString("token", id);
+        editor.putString("name", name);
+        editor.apply();
+    }
+
+    /*쉐어드값 불러오기*/
+    private void loadShared() {
+        SharedPreferences pref = getSharedPreferences("profile", MODE_PRIVATE);
+        token = pref.getString("token", "");
+        name = pref.getString("name", "");
     }
 }
