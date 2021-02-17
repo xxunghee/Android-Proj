84d7c13ff0c40aba37c1c90fcc8977213a1452d3
xxunghee <xxunghee@gmail.com>
2021-02-17 15:27:13 +0900

    21-02-17 KakaoLogin
    사용자 id, 닉네임, 이메일, 프로필 사진 가져오기 추가

--- MyKakaoLogin/app/src/main/java/com/example/mykakaologin/LoginActivity.java
+++ MyKakaoLogin/app/src/main/java/com/example/mykakaologin/LoginActivity.java
@@ -9,6 +9,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.kakao.sdk.auth.LoginClient;
 import com.kakao.sdk.auth.model.OAuthToken;
+import com.kakao.sdk.user.UserApiClient;
+import com.kakao.sdk.user.model.Account;
+import com.kakao.sdk.user.model.Profile;
+import com.kakao.sdk.user.model.User;
 
 import org.jetbrains.annotations.Nullable;
 
@@ -16,6 +20,10 @@ import kotlin.Unit;
 import kotlin.jvm.functions.Function2;
 
 public class LoginActivity extends AppCompatActivity {
+    final String TAG = "LoginActivity";
+
+    private String userName;
+    private Long userId;
 
     final Function2 callback = (Function2)(new Function2() {
         // $FF: synthetic method
@@ -27,13 +35,46 @@ public class LoginActivity extends AppCompatActivity {
 
         public final void invoke(@Nullable OAuthToken token, @Nullable Throwable error) {
             if (error != null) {
-                Log.e("LoginActivity", "Kakao Login Failed :", error);
+                Log.e(TAG, "Kakao Login Failed :", error);
             } else if (token != null) {
-                Log.d("LoginActivity", "Kakao Login Success");
-                LoginActivity.this.startMainActivity();
+                Log.d(TAG, "Kakao Login Success");
+                // 로그인 성공 시 사용자 정보 가져오기
+                getUserInfo();
             }
         }
     });
+
+    // 사용자 정보 가져오기
+    public void getUserInfo() {
+        UserApiClient.getInstance().me(new Function2() {
+            // $FF: synthetic method
+            // $FF: bridge method
+            public Object invoke(Object var1, Object var2) {
+                this.invoke((User)var1, (Throwable)var2);
+                return Unit.INSTANCE;
+            }
+
+            public final void invoke(@Nullable User user, @Nullable Throwable error) {
+                if (error != null) {
+                    Log.e(TAG, "사용자 정보 요청 실패", error);
+                } else if (user != null) {
+                    Log.d(TAG, "사용자 정보 요청 성공");
+                    userId = user.getId();
+                    Account userKakaoAccount = user.getKakaoAccount();
+                    if (userKakaoAccount != null) {
+                        Profile userProfile = userKakaoAccount.getProfile();
+                        if (userProfile != null) {
+                            userName = userProfile.getNickname();
+                            startMainActivity();
+                        }
+                    }
+                    else
+                        userName = null;
+                    Log.d(TAG, userName + " / " + userId);
+                }
+            }
+        });
+    }
 
     private void startMainActivity(){
         Intent intent = new Intent(getApplicationContext(), MainActivity.class);
